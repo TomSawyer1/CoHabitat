90e622631553a4d085366490c199147dd3f06992
TomSawyer1 <117282498+TomSawyer1@users.noreply.github.com>
2025-06-06 01:34:00 +0200

    Nettoyage: Mise à jour des fichiers de configuration du projet

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./app/**/*.{js,jsx,ts,tsx}",
-    "./components/**/*.{js,jsx,ts,tsx}"
-  ],
+  content: ["./app/**/*.{js,jsx,ts,tsx}", "./components/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {
       colors: {
-        primary: '#000000',
-        secondary: '#FFFFFF',
+        primary: "#000000",
+        secondary: "#FFFFFF",
       },
     },
   },
   plugins: [],
-} 
+};
